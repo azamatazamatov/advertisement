2bfe95dd9a4a35df2b80254d51210e7e2f44fb7d
Azamat <azamat_azamatov@epam.com>
2024-12-17 10:41:10 +0500

    added launch.json

--- middlewares/user.py
+++ middlewares/user.py
@@ -10,7 +10,6 @@ class APIKeyMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
         api_key = request.headers.get('X-API-Key')
         if api_key is None:
-            # If API key is not present in the request header, return 401
             print("API key required")
         response = await call_next(request)
         return response
