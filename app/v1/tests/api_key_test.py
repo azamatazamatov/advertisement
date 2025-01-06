299e7e7aea8fe8647d80febe4ec4708f0a9c3c41
Azamat <azamat_azamatov@epam.com>
2025-01-06 14:29:33 +0500

    refactor: removed API_KEY variable from test

--- app/v1/tests/api_key_test.py
+++ app/v1/tests/api_key_test.py
@@ -7,26 +7,17 @@ client = TestClient(app)
 
 def test_with_correct_api_key():
     with patch.dict(os.environ, {"API_KEY": "test"}):
-        # Arrange
-        API_KEY = os.environ.get("API_KEY")
+        response = client.get("/", headers={"X-API-Key": "test"})
 
-        # Act
-        response = client.get("/", headers={"X-API-Key": API_KEY})
-
-        # Assert
         assert response.status_code != 401
     
 def test_with_wrong_api_key():
     with patch.dict(os.environ, {"API_KEY": "test"}):
-        # Act
         response = client.get("/", headers={"X-API-KEY": "1234"})
         
-        # Assert
         assert response.status_code == 401
 
 def test_without_api_key():
-    # Act
     response = client.get("/")
     
-    # Assert
     assert response.status_code == 401
